5a68bbf2bdaba6d1381bda44502438c590ef38df
jeraldsiby <jeraldsiby05@gmail.com>
2020-09-21 19:49:26 -0400

    in-memory db changed to local db

---
+++ WebAPI/Migrations/20200921234519_Initial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Address = table.Column<string>(nullable: true),
+                    PostalCode = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    CallDuration = table.Column<TimeSpan>(nullable: false),
+                    Response = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -27,8 +27,12 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<TeleHealthDbContext>(opt =>
-               opt.UseInMemoryDatabase("TeleHealthDb"));
+            //services.AddDbContext<TeleHealthDbContext>(opt =>
+            //   opt.UseInMemoryDatabase("TeleHealthDb"));
+
+            services.AddDbContext<TeleHealthDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("TeleHealthApp")));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             //registering the Repository with the Dependency Injection System
